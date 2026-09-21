6f723da7ee37325b9c1b95bb4ef45a9f5c105185
shiven <shiven.u.2004@gmail.com>
2026-09-21 21:50:23 +0530

    made GPIO size parameterized

--- gpio.v
+++ gpio.v
@@ -2,20 +2,22 @@
  * GPIO Module
  * Supports bidirectional GPIO functionality with output enable control.
  */
-module aio_gpio (
+module aio_gpio #(
+    parameter integer GPIO_WIDTH = 8
+) (
     input  wire       clk,
 
-    input  wire [7:0] gpio_out,
-    input  wire [7:0] gpio_oe,
-    output wire [7:0] gpio_in,
+    input  wire [GPIO_WIDTH-1:0] gpio_out,
+    input  wire [GPIO_WIDTH-1:0] gpio_oe,
+    output wire [GPIO_WIDTH-1:0] gpio_in,
 
-    inout  wire [7:0] gpio
+    inout  wire [GPIO_WIDTH-1:0] gpio
 );
 
 genvar i;
 
 generate
-    for (i = 0; i < 8; i = i + 1) begin
+    for (i = 0; i < GPIO_WIDTH; i = i + 1) begin
         assign gpio[i] = gpio_oe[i] ? gpio_out[i] : 1'bz;
         assign gpio_in[i] = gpio[i];
     end
